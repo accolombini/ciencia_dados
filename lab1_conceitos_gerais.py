2cca126d81384f6c81fa9f1cfe6183dc2aad9b71
accolombini <accolombini@gmail.com>
2020-05-19 10:49:00 -0300

    fundamentos de estatistica

---
+++ lab1_conceitos_gerais.py
@@ -0,0 +1,10 @@
+# Estatistica --> Ciencia que usa de teorias em experimentos e observacoes para o estudo destes,divide-se em:
+# 1- Descritiva => organizar, demonstrar e resumir dados
+# 2- Probabilidade => analisar situações sujeitas ao acaso
+# 3- Inferência => obter respostas sobre um fenômeno com dados representativos
+# Observacao e experimento:
+# 1- Observacao => estudo em que os elementos analisados nao sao afetados (pesquisa de intencao de votos, por exemplo) <> nao ha como controlar uma relacao de causa e efeito, por exemplo
+# 2- Experimentos => condicoes ou tratamentos sao impostas a grupos, para avaliar o resultado (nete caso o pesquisador interfere no processo, por exmplo definindo algumas condicoes) <> neste caso e possivel controlar uma relacao entre causa e feito
+# Quando o assunto e Estatitistica podemos definir Variaveis estabelecendo a seguinte divisao:
+# 1- Quantitativa => numericas, podendo ser: continuas >>= valores reais, podem assumir qualquer intervalo e => discretas >>= numeros fixos, inteiro num intervalo
+# 2- Qualitativas => tambem chamadas de categoricas, podendo ser: nominais >>= categorias sem hieraquia => ordinais >>= categorias com hierarquia
