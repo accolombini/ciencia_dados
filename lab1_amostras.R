f6d97f9befd28f8a711b8158fd1ce2cfec88e892
accolombini <accolombini@gmail.com>
2020-05-20 15:09:05 -0300

    amostras em R parte 1

---
+++ lab1_amostras.R
@@ -0,0 +1,6 @@
+# Title     : Amostras
+# Objective : Como o R pode nos auxiliar para trabalhar com amostras
+# Created by: accol
+# Created on: 19/05/2020
+
+dim(iris) # visualizar a dimensao da base de dados
